f833d0e5cc3540fdd6cef358a7cd09daf168f6f1
Akiko Green <agreen@alphaworks.tech>
2021-10-20 13:35:33 -0700

    test(add missing test): added a test to validate if time formatting is correct

--- application/src/redux/store.js
+++ application/src/redux/store.js
@@ -3,6 +3,6 @@ import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
 
-const store = createStore(reducers, {}, compose(applyMiddleware(ReduxThunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
 export default store;
